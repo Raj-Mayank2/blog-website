34e65ee76063b2612dcc61c411a0766150c7244d
Raj-Mayank2 <mayank170403@gmail.com>
2025-07-05 11:46:49 +0530

    second commit

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,8 @@
+// src/api/axios.js
 import axios from 'axios';
 
-const instance=axios.create({
-    baseURL:'http://localhost:5000/api'
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default instance;
